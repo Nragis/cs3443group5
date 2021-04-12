536e5beccc3bd0630c23979bd84080dea9777ac8
Nragis <quinn_murphey@me.com>
2021-04-12 13:52:42 -0500

    Added all fxml files and their corresponding controllers with fx:id already set

---
+++ src/main/java/bullet_hell/controller/MainMenuController.java
@@ -0,0 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuController {
+
+    @FXML
+    private Button startGameButton;
+
+    @FXML
+    private Button statisticsButton;
+
+    @FXML
+    private Button optionsButton;
+
+    @FXML
+    private Button quitButton;
+
+}
---
+++ src/main/java/bullet_hell/controller/StatisticsMenuController.java
@@ -0,0 +1,52 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class StatisticsScreenController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label highScoreName1;
+
+    @FXML
+    private Label highScoreNumber1;
+
+    @FXML
+    private Label highScoreName2;
+
+    @FXML
+    private Label highScoreName3;
+
+    @FXML
+    private Label highScoreName4;
+
+    @FXML
+    private Label highScoreName5;
+
+    @FXML
+    private Label highScoreNumber2;
+
+    @FXML
+    private Label highScoreNumber3;
+
+    @FXML
+    private Label highScoreNumber4;
+
+    @FXML
+    private Label highScoreNumber5;
+
+    @FXML
+    private Label attemptsLabel;
+
+    @FXML
+    private Label timePlayedLabel;
+
+    @FXML
+    private Label bulletsFiredLabel;
+
+    @FXML
+    private Label stagesCompletedLabel;
+
+}
