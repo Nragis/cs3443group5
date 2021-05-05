00957563f45bf7bfec807e0c65ce0acb06104144
Nragis <quinn_murphey@me.com>
2021-05-05 15:41:54 -0700

    Caused the jar to run server when given server argument

--- src/main/java/bullet_hell/Launcher.java
+++ src/main/java/bullet_hell/Launcher.java
@@ -1,7 +1,13 @@
 package bullet_hell;
 
+import bullet_hell.remote.server.LeaderboardServerImpl;
+
 public class Launcher{
     public static void main(String[] args){
-        Main.main(args);
+		if(args.length > 0 && args[0].equals("server") ){
+			LeaderboardServerImpl.main(args);
+		} else{
+			Main.main(args);
+		}
     }
 }
